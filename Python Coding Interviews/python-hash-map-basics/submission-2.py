4c72dea6a65ff9cb7ead6952f7160001e762aa61
Miguel Vilhena <57013264+mvmories@users.noreply.github.com>
2026-09-24 13:08:54 +0200

    Add: python-hash-map-basics - submission-2

---
+++ Python Coding Interviews/python-hash-map-basics/submission-2.py
@@ -0,0 +1,23 @@
+from typing import List, Dict
+
+
+def build_hash_map(keys: List[str], values: List[int]) -> Dict[str, int]:
+    output = {}
+    for k, v in zip(keys, values):
+    # we assume the lists are ordered and that 
+    # index of keys maps to index of values
+        output[k] = v    
+    return output
+
+def get_values(hash_map: Dict[str, int], keys: List[str]) -> List[int]:
+    return [hash_map[key] for key in keys if key in hash_map]
+
+
+# do not modify below this line
+print(build_hash_map(["Alice", "Bob", "Charlie"], [90, 80, 70]))
+print(build_hash_map(["Jane", "Carol", "Charlie"], [25, 100, 60]))
+print(build_hash_map(["Doug", "Bob", "Tommy"], [80, 90, 100]))
+
+print(get_values({"Alice": 90, "Bob": 80, "Charlie": 70}, ["Alice", "Bob", "Charlie"]))
+print(get_values({"Jane": 25, "Charlie": 60, "Carol": 100, }, ["Jane", "Carol"]))
+print(get_values({"X": 205, "Y": 78, "Z": 100}, ["Y"]))
